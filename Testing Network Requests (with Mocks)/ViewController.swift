b136cb14eef70d0eaa2113f88ba5f2beba49355d
Jake <jlconnerly@gmail.com>
2020-08-25 13:22:59 -0500

    decoupled our session var from URLSession

--- Testing Network Requests (with Mocks)/ViewController.swift
+++ Testing Network Requests (with Mocks)/ViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
+protocol URLSessionProtocol {
+    // in order to call the dataTask function, we had to pull this from the original URLSession
+    // then add it to our protocol URLSessionProtocol in order to call dataTask
+    // in doing this, we decoupled session and URLSession which is a difficult dependency to test
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var sendRequestButton: UIButton!
     private var dataTask: URLSessionDataTask?
+    // var session had an initial implicit type of URLSession
+    // now we are adding the explicit type URLSessionProtocol
+    var session: URLSessionProtocol = URLSession.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +36,7 @@ class ViewController: UIViewController {
         guard let encodedTerms = terms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
             let url = URL(string: "https://itunes.apple.com/search?" + "media=ebook&term=\(encodedTerms)") else { return }
         let request = URLRequest(url: url)
-        dataTask = URLSession.shared.dataTask(with: request) {
+        dataTask = session.dataTask(with: request) {
             [weak self] (data: Data?, response: URLResponse?, error: Error?)
             -> Void in
             guard let self = self else { return }
@@ -45,6 +55,5 @@ class ViewController: UIViewController {
         sendRequestButton.isEnabled = false
         dataTask?.resume()
     }
-    
 }
-
+extension URLSession: URLSessionProtocol { }
