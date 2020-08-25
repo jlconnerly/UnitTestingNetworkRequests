74c6a3eb222e64d7d8ecdc8cf9f6ad206ca3beb6
Jake <jlconnerly@gmail.com>
2020-08-25 12:42:45 -0500

    Inital Commit

---
+++ Testing Network Requests (with Mocks)/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Testing Network Requests (with Mocks)
+//
+//  Created by Jake Connerly on 8/25/20.
+//  Copyright © 2020 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Testing Network Requests (with Mocks)Tests/Testing_Network_Requests__with_Mocks_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Testing_Network_Requests__with_Mocks_Tests.swift
+//  Testing Network Requests (with Mocks)Tests
+//
+//  Created by Jake Connerly on 8/25/20.
+//  Copyright © 2020 jake connerly. All rights reserved.
+//
+
+import XCTest
+@testable import Testing_Network_Requests__with_Mocks_
+
+class Testing_Network_Requests__with_Mocks_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
